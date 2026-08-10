9557774e3203e650fcbba0232931010de71e8def
Maradan Tom <maradantom@gmail.com>
2026-08-10 18:36:16 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'flip7-v1';
+const FILES = [
+  '/flip7/',
+  '/flip7/index.html',
+  '/flip7/manifest.json',
+  '/flip7/icon-192.png',
+  '/flip7/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
